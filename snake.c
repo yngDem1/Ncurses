80d4aebf9a2abad727c5bc0ff4373a7c47e353eb
Arteezy <61015986+yngDem1@users.noreply.github.com>
2020-07-02 20:58:28 +0300

    Update snake.c
    Голова змейки полностью забила на функцию isGameOver и для нее не существует границ, в которых она может существовать. Требует изменений функция с появлением еды на поле. Seems like проект уже приобрел форму, но еще пара-тройка часов дошлифовки и переработки кода.

--- snake.c
+++ snake.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-void isGameOver(int x, int snakeX, int y, int snakeY, bool gameOver)
+void isGameOver(int x, int snakeX, int y, int snakeY, bool gameOver/*, int snakeLength, int *tailX, int *tailY*/)
 {
+      /* for(int i = 0; i < snakeLength;i++)
+       if(tailX[i] == snakeX || tailY[i] == snakeY)
+gameOver = true; */
     if (snakeX >= x || snakeX <= 0 || snakeY >= y || snakeY <= 0)
+    /* if(snakeX >= x+1) snakeX = 0;
+    else if(snakeX < 0) snakeX = x;
+    else if(snakeY >= y+1) snakeY = 0;
+    else if(snakeY < 0) snakeY = y; */
     gameOver = true;  
 }
 
 void foodSpawn(int x, int y, int foodX, int foodY, int foodValue, bool foodEaten)
 {
-    if (foodEaten)
+    if (!foodEaten)
     {
     srand(time(0)); // seed for random generator
     foodX = (rand() % x) + 1;
@@ -75,18 +82,19 @@ int main() {
     noecho();
     keypad(stdscr, 1); // keypad initialization
     
-    while(gameOver) // main cycle
+    while(!gameOver)
 {
     erase();
 
+    isGameOver(x, y, snakeX, snakeY, &gameOver);
     snakeSpawn(x, y, snakeX, snakeY, snakeLength);
-    foodSpawn(x, y, foodX, foodY, foodValue, &foodEaten);
     snakeMovement(&tailX[1000], &tailY[1000], snakeX, snakeY, snakeLength);
+    foodSpawn(x, y, foodX, foodY, foodValue, &foodEaten);
     eatingFood(snakeX, snakeY, snakeLength, foodX, foodY, &foodEaten, foodValue, score);
 
     refresh();
 
-            // mvprintw(y, x, "S");
+            mvprintw(snakeY, snakeX, "S");
             
             int ch = getch();
             if (ch == KEY_UP)
